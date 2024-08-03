dfc2a47e431c63bcd3eafdc60730ad033515cc8f
4dr3s <andres30092000@hotmail.com>
2024-08-03 11:49:01 -0500

    Recorre todo el json

--- pythonProject1/code/transform.py
+++ pythonProject1/code/transform.py
@@ -5,7 +5,7 @@ import soundfile as sf
 from scipy.signal import resample
 import pandas as pd
 import json
-file_path = "C:/Users/andre/Downloads/datos_tiktok2.json"
+file_path = "C:/Users/andre/Downloads/datos_tiktok_threads_instagram_twitter.json"
 with open(file_path, 'r', encoding='utf-8') as f:
     data = json.load(f)
 df = pd.json_normalize(data)
@@ -18,27 +18,26 @@ def change_description(description):
 path = 'C:/Universidad/Legislacion/videos/'
 for i, item in df.iterrows():
     try:
-        video_path = path + f"{item['id']}.mp4"
-        video_file = mp.VideoFileClip(video_path)
-        audio_path = path + f"{item['id']}.mp3"
-        video_file.audio.write_audiofile(audio_path)
-        data, sample_rate = sf.read(audio_path)
-        if sample_rate != 16000:
-            data = resample(data, int(len(data) * 16000 / sample_rate))
-        sf.write(audio_path, data, 16000)
-        model = SpeechRecognitionModel("jonatasgrosman/wav2vec2-large-xlsr-53-spanish")
-        audio_paths = [audio_path]
-        transcriptions = model.transcribe(audio_paths)
-        if transcriptions:
-            transcription_text = transcriptions[0]['transcription']
-            df.at[i, "description"] = change_description(transcription_text)
-        if i == 0:
-            break
+        if item['fuente'] == 'Tik Tok':
+            video_path = path + f"{item['id']}.mp4"
+            video_file = mp.VideoFileClip(video_path)
+            audio_path = path + f"{item['id']}.mp3"
+            video_file.audio.write_audiofile(audio_path)
+            data, sample_rate = sf.read(audio_path)
+            if sample_rate != 16000:
+                data = resample(data, int(len(data) * 16000 / sample_rate))
+            sf.write(audio_path, data, 16000)
+            model = SpeechRecognitionModel("jonatasgrosman/wav2vec2-large-xlsr-53-spanish")
+            audio_paths = [audio_path]
+            transcriptions = model.transcribe(audio_paths)
+            if transcriptions:
+                transcription_text = transcriptions[0]['transcription']
+                df.at[i, "description"] = change_description(transcription_text)
     except Exception as ex:
         print(ex)
         continue
 
 modified_data = df.to_dict(orient='records')
-output_file_path = "C:/Users/andre/Downloads/datos_tiktok3.json"
+output_file_path = "C:/Users/andre/Downloads/datos_tiktok_threads_instagram_twitter_get_video_description.json"
 with open(output_file_path, 'w', encoding='utf-8') as f:
     json.dump(modified_data, f, ensure_ascii=False, indent=4)
