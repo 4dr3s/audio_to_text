929b3dcb3e93105b1a3516866e2c6eef4be44009
4dr3s <andres30092000@hotmail.com>
2024-08-03 21:45:35 -0500

    requirements.txt

--- pythonProject1/code/transform.py
+++ pythonProject1/code/transform.py
@@ -1,7 +1,7 @@
 # Python 3.9
 # Se necesita multiprocesamiento
 import moviepy.editor as mp
-from huggingsound import SpeechRecognitionModel
+# from huggingsound import SpeechRecognitionModel
 import soundfile as sf
 from scipy.signal import resample
 import pandas as pd
@@ -19,11 +19,11 @@ def change_description(description):
 
 
 path = 'C:/Universidad/Legislacion/videos/'
-model = SpeechRecognitionModel("jonatasgrosman/wav2vec2-large-xlsr-53-spanish")
+# model = SpeechRecognitionModel("jonatasgrosman/wav2vec2-large-xlsr-53-spanish")
 for i, item in df.iterrows():
     try:
         if item['fuente'] == 'Tik Tok':
-            if item['id'] == 1:
+            if 36 <= item['id'] <= 100:
                 video_path = path + f"{item['id']}.mp4"
                 audio_path = path + f"Audios/{item['id']}.mp3"
                 if not os.path.exists(audio_path):
@@ -34,15 +34,13 @@ for i, item in df.iterrows():
                 if sample_rate != 16000:
                     data = resample(data, int(len(data) * 16000 / sample_rate))
                 sf.write(audio_path, data, 16000)
-                audio_paths = [audio_path]
-                transcriptions = model.transcribe(audio_paths)
-                if transcriptions:
-                    transcription_text = transcriptions[0]['transcription']
-                    df.at[i, "description"] = change_description(transcription_text)
+                # audio_paths = [audio_path]
+                # transcriptions = model.transcribe(audio_paths)
+                # if transcriptions:
+                #     transcription_text = transcriptions[0]['transcription']
+                #     df.at[i, "description"] = change_description(transcription_text)
                 del video_file, data
                 gc.collect()
-            else:
-                break
     except Exception as ex:
         print(ex)
         continue
